1f9b687192a8c5e29ab039c149e02df4de9f2386
Matthew Palmer <matthewpalmer9@outlook.com>
2022-03-29 13:35:19 -0500

    Established a basic connection between server and client for streaming.

--- dream-stream-client/dream-stream/src/App.js
+++ dream-stream-client/dream-stream/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="videoPlayer" controls muted="muted" autoPlay>
+        <source 
+          src="http://localhost:3000/video/big-buck" type="video/mp4"
+        />
+      </video>
     </div>
   );
 }
---
+++ dream-stream-server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.send(200, "Success!")
+})
+
+app.get("/video/big-buck", (req, res) => {
+    const range = req.headers.range;
+    if(!range) { res.status(400).send("Requires Range header") };
+
+    const videoPath = "videos/bigbuck.mp4";
+    const videoSize = fs.statSync("videos/bigbuck.mp4").size;
+
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+
+    // replace non-digit characters with ""
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1); // videoSize - 1 so that we can't go past it
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    }
+
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    videoStream.pipe(res);
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
