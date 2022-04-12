904b353e6bf0cd645168c561a5aec93d1caa5d5a
Matthew Palmer <matthewpalmer9@outlook.com>
2022-04-12 11:41:32 -0500

    Adjusted some styles for the video page and made the films page responsive.

--- dream-stream-client/dream-stream/src/App.js
+++ dream-stream-client/dream-stream/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import API from "./services/api";
 import VideoPage from "./pages/VideoPage.js";
 import Landing from "./pages/Landing/Landing.js";
@@ -54,9 +54,7 @@ function App() {
     return (
         <div className="App">
             <>
-                <ToastContainer 
-                    draggable={false}
-                />
+                <ToastContainer draggable={false} />
             </>
             <Router>
                 <Navbar
@@ -82,31 +80,74 @@ function App() {
                         }
                     />
                     <Route exact path="/logout" element={<Logout />} />
-                    <Route exact path="/films" element={<Films  />} />
+                    <Route exact path="/films" element={<Films />} />
                     <Route
                         exact
                         path="films/elephantsdream"
-                        element={<VideoPage cookies={cookies} src="elephantsdream" />}
+                        element={
+                            <VideoPage
+                                cookies={cookies}
+                                src="elephantsdream"
+                                videoTitle={"Elephant's Dream"}
+                                videoDescription={
+                                    "Elephants Dream (code-named Project Orange during production and originally titled Machina) is a 2006 Dutch computer animated science fiction fantasy experimental short film produced by Blender Foundation using, almost exclusively, free and open-source software. The film is English-language and includes subtitles in over 30 languages."
+                                }
+                            />
+                        }
                     />
                     <Route
                         exact
                         path="films/bigbuck"
-                        element={<VideoPage cookies={cookies} src="bigbuck" />}
+                        element={
+                            <VideoPage
+                                cookies={cookies}
+                                src="bigbuck"
+                                videoTitle={"Big Buck Bunny"}
+                                videoDescription={
+                                    "Big Buck Bunny (code-named Project Peach) is a 2008 short computer-animated comedy film featuring animals of the forest, made by the Blender Institute, part of the Blender Foundation. Like the foundation's previous film, Elephants Dream, the film was made using Blender, a free and open-source software application for 3D computer modeling and animation developed by the same foundation. Unlike that earlier project, the tone and visuals departed from a cryptic story and dark visuals to one of comedy, cartoons, and light-heartedness."
+                                }
+                            />
+                        }
                     />
                     <Route
                         exact
                         path="films/sintel"
-                        element={<VideoPage cookies={cookies} src="sintel" />}
+                        element={
+                            <VideoPage
+                                cookies={cookies}
+                                src="sintel"
+                                videoTitle={"Sintel"}
+                                videoDescription={
+                                    "Sintel, code-named Project Durian during production, is a 2010 computer-animated fantasy short film. It was the third Blender 'open movie'. It was produced by Ton Roosendaal, chairman of the Blender Foundation, written by Esther Wouda, directed by Colin Levy, at the time an artist at Pixar and art direction by David Revoy, who is known for Pepper&Carrot an open source webcomic series. It was made at the Blender Institute, part of the Blender Foundation. The plot follows the character, Sintel, who is tracking down her pet Scales, a dragon. Just like the other Blender 'open movies,' the film was made using Blender, a free and open source software application for animation, created and supported by the Blender Foundation."
+                                }
+                            />
+                        }
                     />
                     <Route
                         exact
                         path="films/tearsofsteel"
-                        element={<VideoPage cookies={cookies} src="tearsofsteel" />}
+                        element={
+                            <VideoPage
+                                cookies={cookies}
+                                src="tearsofsteel"
+                                videoTitle={"Tears of Steel"}
+                                videoDescription={
+                                    "Tears of Steel (code-named Project Mango) is a short science fiction film by producer Ton Roosendaal and director/writer Ian Hubert. The film is both live-action and CGI; it was made using new enhancements to the visual effects capabilities of Blender, a free and open-source 3D computer graphics app. Set in a dystopian future, the short film features a group of warriors and scientists who gather at the Oude Kerk in Amsterdam in a desperate attempt to save the world from destructive robots."
+                                }
+                            />
+                        }
                     />
                     <Route
                         exact
                         path="films/nightmarebeforechristmas"
-                        element={<VideoPage cookies={cookies} src="nightmarebeforechristmas" />}
+                        element={
+                            <VideoPage
+                                cookies={cookies}
+                                src="nightmarebeforechristmas"
+                                videoTitle={"The Nightmare Before Christmas"}
+                                videoDescription={"The Nightmare Before Christmas (also known as Tim Burton's The Nightmare Before Christmas) is a 1993 American stop-motion animated musical dark fantasy film directed by Henry Selick (in his feature directorial debut) and produced and conceived by Tim Burton. It tells the story of Jack Skellington, the King of 'Halloween Town' who stumbles upon 'Christmas Town' and becomes obsessed with celebrating the holiday. Danny Elfman wrote the songs and score, and provided the singing voice of Jack"}
+                            />
+                        }
                     />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
--- dream-stream-client/dream-stream/src/pages/VideoPage.js
+++ dream-stream-client/dream-stream/src/pages/VideoPage.js
@@ -4,7 +4,7 @@ import Video from "../components/Video/Video.jsx";
 import "../components/styles/video.css";
 import { toast } from "react-toastify";
 
-export default function VideoPage({ src, cookies }) {
+export default function VideoPage({ src, cookies, videoTitle, videoDescription }) {
     const history = useNavigate();
 
     useEffect(() => {
@@ -25,6 +25,10 @@ export default function VideoPage({ src, cookies }) {
             </div>
             <div className="video-container">
                 <Video src={src} />
+            </div>
+            <div className="video-details">
+                <h1 className="video-title">{videoTitle}</h1>
+                <p className="video-description"><em>{videoDescription}</em></p>
             </div>
         </>
     );
