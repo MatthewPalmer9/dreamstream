20511fef5d8a9c7f5b27a8e060ffb6dd42766eae
Matthew Palmer <matthewpalmer9@outlook.com>
2022-04-12 10:23:08 -0500

    Updated the logout page styling & button functionality to redirect. Added a 'Back to Films' button on the video page.

--- dream-stream-client/dream-stream/src/pages/Logout/Logout.js
+++ dream-stream-client/dream-stream/src/pages/Logout/Logout.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import "./logout.css";
 
 export default function Logout() {
+    const [cookies] = useCookies();
+    const history = useNavigate();
+
+    useEffect(() => {
+        if (cookies.id) {
+            history("/");
+        }
+    }, [cookies.id, history]);
+
+    const handleRedirect = () => {
+        history("/login");
+    };
+
     return (
         <div className="logout-container">
             <h1>You have successfully logged out!</h1>
             <p>
-                To sign back in, <button>please click here.</button>
+                To sign back in,{" "}
+                <button onClick={handleRedirect}>please click here.</button>
             </p>
         </div>
     );
--- dream-stream-client/dream-stream/src/pages/VideoPage.js
+++ dream-stream-client/dream-stream/src/pages/VideoPage.js
@@ -1,23 +1,31 @@
 import React, { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import Video from "../components/Video/Video.jsx";
 import "../components/styles/video.css";
 import { toast } from "react-toastify";
 
 export default function VideoPage({ src, cookies }) {
-
     const history = useNavigate();
 
     useEffect(() => {
-        if(!cookies.id) { 
-            toast.error('You need to log in first!');
+        if (!cookies.id) {
+            toast.error("You need to log in first!");
             history("/login");
         }
-    }, [cookies.id, history])
+    }, [cookies.id, history]);
+
+    const handleRedirect = () => {
+        history("/films");
+    };
 
     return (
-        <div className="video-container">
-            <Video src={src} />
-        </div>
+        <>
+            <div className="navigate-back-bar">
+                <button onClick={handleRedirect}>&larr; Back to Films</button>
+            </div>
+            <div className="video-container">
+                <Video src={src} />
+            </div>
+        </>
     );
 }
